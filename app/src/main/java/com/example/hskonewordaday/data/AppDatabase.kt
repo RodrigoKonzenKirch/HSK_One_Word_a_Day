c8a89e93b79a85d4b4472d8388f54ef0ac5ac24a
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2025-03-07 16:45:15 -0300

    Initialize Room database and populate it with data
    The Room database is initialized to provide local data persistence.
    A callback was added to populate the database on creation.
    Data is read from the `hsk1to6` resource file.
    The database is populated with `ChineseWordEntity` data parsed from the resource file.
    A `destroyInstance` function has been added.

--- app/src/main/java/com/example/hskonewordaday/data/AppDatabase.kt
+++ app/src/main/java/com/example/hskonewordaday/data/AppDatabase.kt
@@ -4,6 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.example.hskonewordaday.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 @Database(entities = [ChineseWordEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
@@ -22,13 +29,57 @@ abstract class AppDatabase : RoomDatabase() {
                     DATABASE_NAME
                 )
                     .fallbackToDestructiveMigration()
+                    .addCallback(object : Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+
+                            populateDatabase(context)
+                        }
+                    })
                     .build()
                 INSTANCE = instance
                 instance
             }
         }
+
+        fun populateDatabase(context: Context) {
+            CoroutineScope(Dispatchers.IO).launch {
+                val chineseWordDao = getDatabase(context).chineseWordDao()
+
+                if (chineseWordDao.getWordCount() >= 0) {
+                    val resourceId = R.raw.hsk1to6
+                    val chineseWords = mutableListOf<ChineseWordEntity>()
+
+                    val inputStream = context.resources.openRawResource(resourceId)
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+
+                    reader.useLines { lines ->
+                        lines.forEach { line ->
+                            val columns = line.split('\t')
+                            if (columns.size == 6) {
+                                chineseWords.add(
+                                    ChineseWordEntity(
+                                        id = 0,
+                                        hskLevel = columns[0],
+                                        chineseSimplified = columns[1],
+                                        chineseTraditional = columns[2],
+                                        pronunciationNumber = columns[3],
+                                        pronunciationSymbol = columns[4],
+                                        meaning = columns[5]
+                                    )
+                                )
+                            }
+                        }
+                    }
+                    chineseWordDao.insertAll(chineseWords.toList())
+                }
+            }
+        }
+
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+
     }
-
-
 
 }
