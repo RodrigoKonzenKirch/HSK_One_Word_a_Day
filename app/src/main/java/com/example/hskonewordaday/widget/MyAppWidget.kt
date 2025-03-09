5b1a6ba4605ad8ec4b79e4bb8d464e1d52dcd9ad
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2025-03-09 12:02:50 -0300

    Refactor widget to use Glance state
    The widget was refactored to rely on Glance state for updates. `rememberCoroutineScope` was added for coroutine management, and `GlanceTheme` was added for styling. `provideGlance` method was updated to use `by` operator with `rememberSaveable` to hold the word. `MyContent` was updated to receive the current `ChineseWordEntity` and `onClick` updates the word in Glance state. Remove unused imports.

--- app/src/main/java/com/example/hskonewordaday/widget/MyAppWidget.kt
+++ app/src/main/java/com/example/hskonewordaday/widget/MyAppWidget.kt
@@ -2,15 +2,18 @@ package com.example.hskonewordaday.widget
 
 import android.content.Context
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
@@ -24,32 +27,32 @@ import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
 import com.example.hskonewordaday.data.AppDatabase.Companion.getDatabase
 import com.example.hskonewordaday.data.ChineseWordEntity
-import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MyAppWidget : GlanceAppWidget() {
 
-    @OptIn(DelicateCoroutinesApi::class)
     override suspend fun provideGlance(context: Context, id: GlanceId) {
 
         val wordDao = getDatabase(context).chineseWordDao()
         val randomWord = wordDao.getRandomWord()
 
         provideContent {
-            val hanzi = rememberSaveable { mutableStateOf(randomWord) }
-            MyContent(
-                hanzi,
-                modifier = GlanceModifier.fillMaxSize(),
-                onClick = {
-                    GlobalScope.launch(Dispatchers.IO) {
-                        hanzi.value = wordDao.getRandomWord()
+            var hanzi by rememberSaveable { mutableStateOf(randomWord) }
+            val scope = rememberCoroutineScope()
 
+            GlanceTheme{
+                MyContent(
+                    hanzi,
+                    modifier = GlanceModifier.fillMaxSize(),
+                    onClick = {
+                        scope.launch(Dispatchers.IO) {
+                            hanzi = wordDao.getRandomWord()
+
+                        }
                     }
-                }
-
-            )
+                )
+            }
         }
     }
 
@@ -57,7 +60,7 @@ class MyAppWidget : GlanceAppWidget() {
 
 @Composable
 private fun MyContent(
-    hanzi: State<ChineseWordEntity>,
+    hanzi: ChineseWordEntity,
     modifier: GlanceModifier = GlanceModifier,
     onClick: () -> Unit = {}
 ){
@@ -66,27 +69,31 @@ private fun MyContent(
         verticalAlignment = Alignment.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        // Simplified and Traditional
         Text(
-            text = "${hanzi.value.chineseSimplified} [${hanzi.value.chineseTraditional}]",
+            text = "${hanzi.chineseSimplified} [${hanzi.chineseTraditional}]",
             style = TextStyle(
                 color = ColorProvider(color = Color.White),
                 fontSize = 36.sp,
             )
         )
+        // Pronunciation
         Text(
-            text = "${hanzi.value.pronunciationSymbol} [${hanzi.value.pronunciationNumber}]",
+            text = "${hanzi.pronunciationSymbol} [${hanzi.pronunciationNumber}]",
             style = TextStyle(
                 color = ColorProvider(color = Color.White),
                 ),
             modifier = GlanceModifier.padding(12.dp)
         )
+        // Meaning
         Text(
-            text = hanzi.value.meaning,
+            text = hanzi.meaning,
             style = TextStyle(
                 color = ColorProvider(color = Color.White),
             ),
             modifier = GlanceModifier.padding(12.dp)
         )
+        // Refresh button
         Row(horizontalAlignment = Alignment.CenterHorizontally) {
             Button(
                 text = "Refresh",
