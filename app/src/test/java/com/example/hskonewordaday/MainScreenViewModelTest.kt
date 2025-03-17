d34eb31eacb7c6794ba5cd21d133893f62458b9b
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2025-03-17 18:11:55 -0300

    Refactor MainScreenViewModelTest
    - Refactored `MainScreenViewModelTest` to verify `MainScreenUiState` instead of individual properties like `allWords`, `isLoading`, and `error`.
    - Updated initial state check to verify `MainScreenUiState.Loading`.
    - Modified word emission test to verify `MainScreenUiState.Success` with the emitted words.
    - Adjusted empty word emission test to verify `MainScreenUiState.Empty`.
    - Updated error handling test to verify `MainScreenUiState.Error` with the appropriate error message.

--- app/src/test/java/com/example/hskonewordaday/MainScreenViewModelTest.kt
+++ app/src/test/java/com/example/hskonewordaday/MainScreenViewModelTest.kt
@@ -2,6 +2,7 @@ package com.example.hskonewordaday
 
 import com.example.hskonewordaday.data.ChineseWordEntity
 import com.example.hskonewordaday.domain.WordsRepository
+import com.example.hskonewordaday.ui.MainScreenUiState
 import com.example.hskonewordaday.ui.MainScreenViewModel
 import com.google.common.truth.Truth.assertThat
 import io.mockk.coEvery
@@ -32,12 +33,10 @@ class MainScreenViewModelTest {
 
     @Test
     fun `Initial state check`() {
-        // Verify that the StateFlow 'allWords' emits an empty list 
+        // Verify that the StateFlow 'allWords' has a Loading state
         // immediately upon initialization of the ViewModel.
 
-        assertThat(viewModel.uiState.value.allWords).isEmpty()
-        assertThat(viewModel.uiState.value.isLoading).isTrue()
-        assertThat(viewModel.uiState.value.error).isNull()
+        assertThat(viewModel.uiState.value).isEqualTo(MainScreenUiState.Loading)
     }
 
     @Test
@@ -54,38 +53,32 @@ class MainScreenViewModelTest {
 
         advanceUntilIdle()
 
-        assertThat(viewModel.uiState.value.allWords).containsExactlyElementsIn(chineseWords)
-        assertThat(viewModel.uiState.value.isLoading).isFalse()
-        assertThat(viewModel.uiState.value.error).isNull()
+        assertThat(viewModel.uiState.value).isEqualTo(MainScreenUiState.Success(chineseWords))
     }
 
     @Test
     fun `Empty words emission`() = runTest {
         // Verify that if the repository emits an empty list, the 'allWords' 
-        // StateFlow is updated to an empty list as well.
+        // StateFlow is updated to empty state as well.
 
         val emptyWords = emptyList<ChineseWordEntity>()
         coEvery { fakeRepository.getAllWords() } returns flowOf(emptyWords)
         val viewModel = MainScreenViewModel(fakeRepository, testDispatcher)
 
         advanceUntilIdle()
-        assertThat(viewModel.uiState.value.allWords).isEmpty()
-        assertThat(viewModel.uiState.value.isLoading).isFalse()
-        assertThat(viewModel.uiState.value.error).isNull()
+
+        assertThat(viewModel.uiState.value).isEqualTo(MainScreenUiState.Empty)
     }
 
     @Test
     fun `Error during words retrieval`() {
         // Test the scenario where an error occurs during the retrieval of words 
-        // from the repository, and ensure the StateFlow is still updated accordingly. (e.g empty list)
+        // from the repository, and ensure the StateFlow holds an Error state with the appropriate message.
 
         val errorToThrow = Exception("Test exception")
         coEvery { fakeRepository.getAllWords() } throws errorToThrow
         val viewModel = MainScreenViewModel(fakeRepository, testDispatcher)
 
-        assertThat(viewModel.uiState.value.allWords).isEmpty()
-        assertThat(viewModel.uiState.value.isLoading).isFalse()
-        assertThat(viewModel.uiState.value.error).isEqualTo("Error loading words")
+        assertThat(viewModel.uiState.value).isEqualTo(MainScreenUiState.Error("Error loading words"))
     }
-
 }
